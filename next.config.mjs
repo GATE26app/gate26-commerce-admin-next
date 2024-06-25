d2420826ceb9a3dda7ae8dd66c111047128570e7
esponition <esponition@gmail.com>
2024-06-25 18:14:24 +0900

    feat: update next config
    - rewrites => redirects

--- next.config.mjs
+++ next.config.mjs
@@ -10,14 +10,24 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
-  async rewrites() {
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/backoffice/:path*',
+  //       destination: 'http://cdbackoffice.gate26.co.kr/:path*',
+  //     },
+  //   ];
+  // },
+
+  async redirects() {
     return [
       {
         source: '/backoffice/:path*',
-        destination: 'http://cdbackoffice.gate26.co.kr/:path*',
+        destination: 'https://cdbackoffice.gate26.co.kr/:path*',
+        permanent: false
       },
     ];
-  },
+  }
 };
 
 export default nextConfig;
