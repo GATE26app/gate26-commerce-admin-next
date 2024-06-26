388229bbd3b6a59e6ae3b22055002145fa049d7d
leeyeun <dpdms0109@naver.com>
2024-06-26 09:15:23 +0900

    Fix:login 페이지 이동 수정

--- src/components/Layout/LayoutForm.tsx
+++ src/components/Layout/LayoutForm.tsx
@@ -4,16 +4,23 @@ import { usePathname, useRouter } from 'next/navigation';
 import React, { useEffect } from 'react';
 import BottomLayout from './BottomLayout';
 import MainLayout from './MainLayout';
+import { useUserZuInfo } from '@/_store/UserZuInfo';
 
 function LayoutForm({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   const router = useRouter();
+  const { userZuInfo } = useUserZuInfo((state) => state);
 
-  // useEffect(() => {
-  //   if (sessionStorage.getItem('mt_id') == null) {
-  //     router.replace('/Login');
-  //   }
-  // }, [pathname]);
+  useEffect(() => {
+    if (
+      userZuInfo.accessToken == null ||
+      userZuInfo.accessToken == '' ||
+      userZuInfo.accessToken == undefined
+    ) {
+      router.replace('/login');
+    }
+  }, [userZuInfo, pathname]);
+
   return (
     <>
       {pathname == '/login' ? (
